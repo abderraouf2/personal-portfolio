96123a7f3521099e2e48a778e504028bbffcd6d7
abderraouf2 <abderraouf.mimoune@inttic.dz>
2023-03-31 05:43:26 +0100

    done

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { icons } from "./icons";
 import styles from "./footer.module.scss";
 import Link from "next/link";
-
+import { Fade } from "react-awesome-reveal";
 export default function Footer() {
   return (
     <footer id="contact" className={styles.footer}>
       <h1 className={styles.emailWrapper}>
+        <Fade>
         <p className={styles.workT}>let's work together</p>
         <a className={styles.email} href="mailto:raoufmim29@gmail.com">
           abderraouf.mimoune@inttic.dz
         </a>
+        </Fade>
       </h1>
       <div className={styles.contact}>
         {icons.map((icon, index) => (
