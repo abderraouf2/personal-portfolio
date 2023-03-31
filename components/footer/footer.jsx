1c4921cf16a4ba4297b7b0ecedca66fc7567f376
abderraouf2 <abderraouf.mimoune@inttic.dz>
2023-03-31 05:24:25 +0100

    trying to fix5

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { icons } from "./icons";
 import styles from "./footer.module.scss";
-import { Fade } from "react-awesome-reveal";
 import Link from "next/link";
 
 export default function Footer() {
@@ -10,7 +9,7 @@ export default function Footer() {
       <h1 className={styles.emailWrapper}>
         <p className={styles.workT}>let's work together</p>
         <a className={styles.email} href="mailto:raoufmim29@gmail.com">
-          <Fade>abderraouf.mimoune@inttic.dz</Fade>
+          abderraouf.mimoune@inttic.dz
         </a>
       </h1>
       <div className={styles.contact}>
